b1186707df9385afe6205c9252ca46c1fe8bcceb
Fernandez <mathieu.fernandez.nantes@gmail.com>
2019-03-28 12:08:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,13 +16,9 @@
  </style>
  </head>
  <body>
- <h1>Register here!</h1>
+ <h1>SUPERVISION</h1>
  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br>
-       Email <input type="text" name="email" id="email"/></br>
-       <input type="submit" name="submit" value="Submit" />
- </form>
+
 <?php 
 	 // DB connection info
 	 $host = "tcp:bddintern.database.windows.net,1433";
