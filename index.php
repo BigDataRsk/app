dbcca31b86f90db44b9d4908d34104b102203a01
Fernandez <mathieu.fernandez.nantes@gmail.com>
2019-03-28 11:56:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,13 +16,7 @@
  </style>
  </head>
  <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br>
-       Email <input type="text" name="email" id="email"/></br>
-       <input type="submit" name="submit" value="Submit" />
- </form>
+ <h1>Supervision</h1>
  <?php
 	 // DB connection info
 	 $host = "tcp:bddintern.database.windows.net,1433";
@@ -59,24 +53,16 @@
 	 }
 
 
-	 $sql_select = "SELECT * FROM registration_tbl";
+	 $sql_select = "SELECT count(*) FROM dossier where traite=0";
 	 $stmt = $conn->query($sql_select);
 	 $registrants = $stmt->fetchAll(); 
-	 if(count($registrants) > 0) {
-	 	echo "<h2>People who are registered:</h2>";
-	 	echo "<table>";
-	 	echo "<tr><th>Name</th>";
-	 	echo "<th>Email</th>";
-	 	echo "<th>Date</th></tr>";
-	 	foreach($registrants as $registrant) {
-	 		echo "<tr><td>".$registrant['name']."</td>";
-	 		echo "<td>".$registrant['email']."</td>";
-	 		echo "<td>".$registrant['date']."</td></tr>";
-	     }
-	  	echo "</table>";
-	 } else {
-	 	echo "<h3>No one is currently registered.</h3>";
-	 }
+	 echo "<h3> DOSSIER NON TRAITE".$registrant"</h3>";
+
+	
+	 $sql_select = "SELECT count(*) FROM dossier where traite=1";
+	 $stmt = $conn->query($sql_select);
+	 $registrants = $stmt->fetchAll(); 
+	 echo "<h3> DOSSIER TRAITE".$registrant"</h3>";
  ?>
  </body>
  </html>	
